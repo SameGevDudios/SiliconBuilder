7857584892c0d3d645223211c133a7a104cff534
SameGevDudios <magomed.chapaev1@gmail.com>
2025-03-12 20:43:54 +0300

    Add pool manager

---
+++ Assets/Scripts/ObjectPooling/PoolManager.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Pool
+    {
+        public string Tag;
+        public GameObject ObjectToPool;
+        public int Size;
+    }
+
+
+    public List<Pool> Pools;
+    public Dictionary<string, Queue<GameObject>> PoolDistionary;
+
+    #region Singleton
+    public static PoolManager Instance;
+    private void Awake() => Instance = this;
+    #endregion
+
+    private void Start()
+    {
+        PoolDistionary = new Dictionary<string, Queue<GameObject>>();
+
+        foreach(Pool pool in Pools)
+        {
+            Queue<GameObject> objectPool = new Queue<GameObject>();
+
+            for (int i = 0; i < pool.Size; i++)
+            {
+                GameObject buffer = Instantiate(pool.ObjectToPool, transform);
+                buffer.SetActive(false);
+                objectPool.Enqueue(buffer);
+            }
+
+            PoolDistionary.Add(pool.Tag, objectPool);
+        }
+    }
+
+    public GameObject InstantiateFromPool(string tag, Vector3 position, Quaternion rotation)
+    {
+        if (!PoolDistionary.ContainsKey(tag))
+        {
+            Debug.LogWarning("Pool with " + tag + " tag doesn't exist");
+            return null;
+        }
+
+        GameObject objectToSpawn = PoolDistionary[tag].Dequeue();
+        objectToSpawn.SetActive(true);
+        objectToSpawn.transform.position = position;
+        objectToSpawn.transform.rotation = rotation;
+
+        PoolDistionary[tag].Enqueue(objectToSpawn);
+
+        return objectToSpawn;
+    }
+}
