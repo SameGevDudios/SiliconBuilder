3cebd02d534860401df4f6f0304c9186f4369649
SameGevDudios <magomed.chapaev1@gmail.com>
2025-03-13 16:15:57 +0300

    Add bootstrapper

---
+++ Assets/Scripts/Bootstrappers/Bootstrapper.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class Bootstrapper : MonoBehaviour
+{
+    [SerializeField] private PlayerInput _playerInput;
+    [SerializeField] private BuildingList _buildingList;
+    [SerializeField] private PoolManager _poolManager;
+    [SerializeField] private GameSettings _gameSettings;
+    [SerializeField] private PlayerActions _playerActions;
+    [SerializeField] private Button[] _buildableButtons;
+    private void Awake()
+    {
+        // Placing
+        IInput input = new DesktopInput(_playerInput);
+        IPlaceValidator validator = new RangeValidator();
+        ISelector selector = new Selector(_buildingList);
+        IPlacer placer = new Placer(selector, _poolManager);
+        IPlaceController placeController = new PlaceController(input, validator, selector, placer, _gameSettings.GridSize);
+
+        // Removing
+        IRemover remover = new Remover(_gameSettings.RemoveMask);
+        IRemoveController removeController = new RemoveController(input, remover);
+
+        // Player Actions
+        _playerActions.Constructor(placeController, removeController, placer);
+
+        // Buttons
+        SetupButtons(selector);
+    }
+    private void SetupButtons(ISelector selector)
+    {
+        for (int i = 0; i < _buildableButtons.Length; i++)
+            _buildableButtons[i].onClick.AddListener(() => selector.SetCurrentBuildable(i));
+    }
+}
