c7ee7be540422af5b6315b7f9c99c37ba0b00d7f
SameGevDudios <magomed.chapaev1@gmail.com>
2025-03-13 16:03:28 +0300

    Add constructor for RemoveController

--- Assets/Scripts/Removing/RemoveController/RemoveController.cs
+++ Assets/Scripts/Removing/RemoveController/RemoveController.cs
@@ -3,6 +3,11 @@ public class RemoveController : IRemoveController
     private IInput _input;
     private IRemover _remover;
 
+    public RemoveController(IInput input, IRemover remover)
+    {
+        _input = input;
+        _remover = remover;
+    }
     public void Update()
     {
         if (_input.CursorDown())
