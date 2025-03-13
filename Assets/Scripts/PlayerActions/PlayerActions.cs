59e7e0e09858e9574302f8f0220086aeb5850c56
SameGevDudios <magomed.chapaev1@gmail.com>
2025-03-13 15:34:46 +0300

    Add class for place-remove switch

---
+++ Assets/Scripts/PlayerActions/PlayerActions.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class PlayerActions : MonoBehaviour
+{
+    private IPlaceController _placeController;
+    private IRemoveController _removeController;
+    private IPlacer _placer;
+    private bool _isPlacing;
+
+    public void Constructor(IPlaceController placeController, IRemoveController removeController, IPlacer placer)
+    {
+        _placeController = placeController;
+        _removeController = removeController;
+        _placer = placer;
+    }
+    private void Update()
+    {
+        if(_isPlacing)
+            _placeController.Update();
+        else
+            _removeController.Update();
+    }
+    public void SwitchToPlace() =>
+        _isPlacing = true;
+    public void SwitchToRemove()
+    {
+        _placer.DisposeCurrentBuildable();
+        _isPlacing = false;
+    }
+}
