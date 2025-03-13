c34d9aca1992d9910fe65ca62ac7ce0f3db33c35
SameGevDudios <magomed.chapaev1@gmail.com>
2025-03-13 14:43:36 +0300

    Add place controller class

---
+++ Assets/Scripts/Placing/PlaceController/PlaceController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceController : IPlaceController
+{
+    private IInput _input;
+    private IPlaceValidator _validator;
+    private ISelector _selector;
+    private IPlacer _placer;
+    private int gridSize;
+
+    public void Update()
+    {
+        if (_validator.CanPlace(_input.CursorWorldPosition(), _selector.GetCurrentBuildable().Size / 2))
+        {
+            if (!_input.CursorOverUI())
+            {
+                _placer.UpdatePosition(GridPosition());
+                if (_input.CursorDown())
+                {
+                    _placer.InstantiateCurrentBuildable();
+                }
+            }
+        }
+    }
+    public Vector3 GridPosition()
+    {
+        Vector3 cursorPosition = _input.CursorWorldPosition();
+        Vector3 gridPosition = new Vector3(
+            (int)(cursorPosition.x / gridSize),
+            (int)(cursorPosition.y / gridSize),
+            (int)(cursorPosition.z / gridSize));
+        return gridPosition;
+    }
+}
