a04529736b7181dc0b2b7dbf57f58d122c2f5a39
SameGevDudios <magomed.chapaev1@gmail.com>
2025-03-24 08:54:23 +0300

    Add support for object pooling configuration

--- Assets/Scripts/ScriptableObjects/Buildings/Building.cs
+++ Assets/Scripts/ScriptableObjects/Buildings/Building.cs
@@ -3,9 +3,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewBuilding", menuName = "Buildings/Building")]
 public class Building : ScriptableObject
 {
+    [Header("Visuals")]
     public Sprite BuildingSprite;
-    public string PoolingName;
     public int Size;
+    [Space(1), Header("Pooling")]
+    public string PoolingName;
+    public GameObject Prefab;
+    public int PoolingSize;
     public float GridOffset =>
         Size % 2 == 0 ? 0 : -0.5f;
 }
